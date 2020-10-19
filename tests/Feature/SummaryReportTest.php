ee0206f49c5a38d11f94f5f61b0f8ee72ffb1bb3
Simon Hansen <simon@sourceboat.com>
2020-10-19 15:46:05 +0200

    add basic test

---
+++ tests/Feature/SummaryReportTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Sourceboat\LaravelClockifyApi\Tests\Feature;
+
+use Illuminate\Support\Facades\Http;
+use Sourceboat\LaravelClockifyApi\Repositories\ClockifyRepository;
+use Sourceboat\LaravelClockifyApi\Tests\TestCase;
+
+class SummaryReportTest extends TestCase
+{
+    private array $userIds = [1, 2, 3];
+
+    public function test(): void
+    {
+        Http::fake();
+
+        ClockifyRepository::makeSummaryReport()
+            ->users($this->userIds)
+            ->get();
+
+        Http::assertSent(function ($request) {
+            return count($request['users']['ids']) === count($this->userIds);
+        });
+    }
+}
