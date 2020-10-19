ad221d15d278623ddfd0de60f6002f6d61ada1a9
Sebastian Müller <sebastian@sourceboat.com>
2020-10-19 12:11:35 +0200

    Add basic repository and reports classes

---
+++ src/Reports/ClockifyDetailedReport.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Sourceboat\LaravelClockifyApi\Reports;
+
+class ClockifyDetailedReport extends ClockifyReport
+{
+
+    protected string $reportEndpoint = '/detailed';
+
+    private int $page = 1;
+
+    private int $pageSize = 50;
+
+    public function get()
+    {
+        return json_decode($this->executeApiCall()->body());
+    }
+
+    public function page(int $page)
+    {
+        $this->page = $page;
+        return $this;
+    }
+
+    public function pageSize(int $pageSize)
+    {
+        $this->pageSize = $pageSize;
+        return $this;
+    }
+
+    protected function requestData()
+    {
+        return $this->filter((array) [
+            'dateRangeStart' => $this->dateRangeStart,
+            'dateRangeEnd' => $this->dateRangeEnd,
+            'detailedFilter' => [
+                'page' => $this->page,
+                'pageSize' => $this->pageSize,
+                'sortColumn' => 'DATE',
+            ],
+            $this->mergeWhen($this->sortOrder !== '', [
+                'sortOrder' => $this->sortOrder,
+            ]),
+            $this->mergeWhen(!is_null($this->userIds), [
+                'users' => [
+                    'ids' => $this->userIds,
+                    'contains' => 'CONTAINS',
+                    'status' => 'ALL',
+                ],
+            ]),
+            $this->mergeWhen(!is_null($this->tagIds), [
+                'tags' => [
+                    'ids' => $this->tagIds,
+                    'containedInTimeentry' => 'CONTAINS',
+                    'status' => 'ALL',
+                ],
+            ]),
+            $this->mergeWhen(!is_null($this->taskIds), [
+                'tasks' => [
+                    'ids' => $this->taskIds,
+                    'containedInTimeentry' => 'CONTAINS',
+                    'status' => 'ALL',
+                ],
+            ]),
+        ]);
+    }
+
+    public static function make()
+    {
+        return new static;
+    }
+
+}
---
+++ src/Reports/ClockifySummaryReport.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Sourceboat\LaravelClockifyApi\Reports;
+
+class ClockifySummaryReport extends ClockifyReport
+{
+
+    protected string $reportEndpoint = '/summary';
+
+    private array $filterGroups = [
+        'USER',
+        'PROJECT',
+        'TIMEENTRY',
+    ];
+
+    public function get()
+    {
+        return json_decode($this->executeApiCall()->body());
+    }
+
+    protected function requestData()
+    {
+        return $this->filter((array) [
+            'dateRangeStart' => $this->dateRangeStart,
+            'dateRangeEnd' => $this->dateRangeEnd,
+            'summaryFilter' => [
+                'groups' => $this->filterGroups,
+            ],
+            $this->mergeWhen($this->sortOrder !== '', [
+                'sortOrder' => $this->sortOrder,
+            ]),
+            $this->mergeWhen(!is_null($this->userIds), [
+                'users' => [
+                    'ids' => $this->userIds,
+                    'contains' => 'CONTAINS',
+                    'status' => 'ALL',
+                ],
+            ]),
+            $this->mergeWhen(!is_null($this->tagIds), [
+                'tags' => [
+                    'ids' => $this->tagIds,
+                    'containedInTimeentry' => 'CONTAINS',
+                    'status' => 'ALL',
+                ],
+            ]),
+            $this->mergeWhen(!is_null($this->taskIds), [
+                'tasks' => [
+                    'ids' => $this->taskIds,
+                    'containedInTimeentry' => 'CONTAINS',
+                    'status' => 'ALL',
+                ],
+            ]),
+        ]);
+    }
+
+    public function filterGroups(array $filterGroups)
+    {
+        $this->filterGroups = $filterGroups;
+        return $this;
+    }
+
+    public static function make()
+    {
+        return new static;
+    }
+
+}
