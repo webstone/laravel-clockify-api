ff25ab8f27e007f0bf43b5a5a3fd24fc201ced92
Sebastian Müller <sebastian@sourceboat.com>
2020-10-19 12:00:00 +0200

    Add composer.json and ServiceProvider

---
+++ src/LaravelClockifyApiServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Sourceboat\LaravelClockifyApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelClockifyApiServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot(): void
+    {
+       // nothing
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register(): void
+    {
+        // nothing
+    }
+
+}
